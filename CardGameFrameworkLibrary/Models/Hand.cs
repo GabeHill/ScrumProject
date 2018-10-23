849e23ccb0182c216a2b15875b55ea6c8730345e
Bryan <broberts@student.neumont.edu>
2018-10-23 15:59:05 -0600

    Added framework classes.

---
+++ CardGameFrameworkLibrary/Models/Hand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameFrameworkLibrary.Models
+{
+    class Hand
+    {
+        public int HandSize { get; set; }
+        public List<Card> Cards { get; set; }
+    }
+}
