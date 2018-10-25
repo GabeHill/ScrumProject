309a63ffc8b237eff4fca0d9de90a0f1e6832e4e
Bryan <broberts@student.neumont.edu>
2018-10-25 14:00:06 -0600

    Added shuffle Method to framework

--- CardGameFrameworkLibrary/Models/Deck.cs
+++ CardGameFrameworkLibrary/Models/Deck.cs
@@ -11,7 +11,30 @@ namespace CardGameFrameworkLibrary.Models
         public List<Card> Cards { get; set; }
 
 
-        public List<Card> DealCards(int amount)
+        public void Shuffle()
+        {
+            int itr = 7;
+            int Picked, Remaining;
+            List<Card> Shuffling = new List<Card>();
+            Random random = new Random();
+            for (int i = 0; i < itr; i++)
+            {
+                Picked = random.Next(10, 30);
+                Shuffling = Cards.GetRange(0, Picked);
+                Cards.RemoveRange(0, Picked);
+
+                while(Shuffling.Count != 0)
+                {
+                    Picked = random.Next(10, Cards.Count - 1);
+                    Remaining = random.Next(1, Shuffling.Count / 3 + 1);
+                    Cards.InsertRange(Picked, Shuffling.GetRange(0, Remaining));
+                    Shuffling.RemoveRange(0, Remaining);
+                }
+            }
+        }
+
+
+        /*public List<Card> DealCards(int amount)
         {
             Random random = new Random();
             List<Card> DealtCards = new List<Card>();
@@ -23,6 +46,6 @@ namespace CardGameFrameworkLibrary.Models
                 Cards.Remove(card);
             }
             return DealtCards;
-        }
+        }*/
     }
 }
