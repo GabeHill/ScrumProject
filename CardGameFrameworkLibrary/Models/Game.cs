a37637d6ab37bc3c3e4e8ed975528a5a04bcfdcd
Bryan <broberts@student.neumont.edu>
2018-10-25 18:36:03 -0600

    Added game model

---
+++ CardGameFrameworkLibrary/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameFrameworkLibrary.Models
+{
+    class Game
+    {
+        public List<Player> Players { get; set; }
+        public Deck Deck { get; set; }
+        public int Pot { get; set; }
+        public Player Dealer { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
+
+
+//what do you think about having a model for the game that has a list of players, the deck of cards, the pot, dealer cards and a few others?
